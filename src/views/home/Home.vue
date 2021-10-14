c194e3b8edf78761defa52ffb021d2bf640a9ab5
ymf <1137626035@qq.com>
2021-10-14 11:06:03 +0800

    轮播图展示和推荐信息展示

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,12 +1,49 @@
 <template>
-  <h2>首页</h2>
+  <div id="home">
+    <nav-bar class="home-navbar">
+      <template v-slot:center><div slot="center">购物街</div></template>
+    </nav-bar>
+    <home-swiper :banners="banners"/>
+    <recommend-view :recommends="recommends"/>
+  </div>
 </template>
 
 <script>
+  import NavBar from 'components/common/navbar/NavBar';
+  import HomeSwiper from './childComps/HomeSwiper'
+  import RecommendView from './childComps/RecommendView'
+
+  import {getHomeMultidata} from 'network/home';
+  
+
   export default {
-    name: "Home"
+    name: "Home",
+    data() {
+      return {
+        banners: [],
+        recommends: [],
+      }
+    },
+    components: {
+      NavBar,
+      HomeSwiper,
+      RecommendView
+    },
+    created() {
+      // 1.请求网页中的多条数据
+      return getHomeMultidata().then(res => {
+        // console.log(res)
+        // 因为从服务器拿到的数据，在这个组件被创建好了以后就会被内存所回收，所以要在这里定义一个data变量，用来保存从服务器拿到的数据，把相应的数据存到数组中即可
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      })
+    },
   }
 </script>
 
 <style scoped>
+.home-navbar {
+  background-color: var(--color-tint);
+  color: #fff;
+}
 </style>
--- vue.config.js
+++ vue.config.js
@@ -9,5 +9,15 @@ module.exports = {
             .set('assets', resolve('src/assets'))
             .set('components', resolve('src/components'))
             .set('views', resolve('src/views'))
+            .set('network', resolve('src/network'))
     },
+    pwa: {
+      iconPaths: {
+        favicon32: 'favicon.ico',
+        favicon16: 'favicon.ico',
+        appleTouchIcon: 'favicon.ico',
+        maskIcon: 'favicon.ico',
+        msTileImage: 'favicon.ico'
+      }
+    }
 }
